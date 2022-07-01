d5e297fe3350d8c89d85c7a9e9dd3257e33b2944
YC <62389359+yc62897441@users.noreply.github.com>
2022-07-01 10:36:34 +0800

    Delete src directory

--- src/components/AdminNav.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="mb-3">
-    <h1>餐廳後台</h1>
-    <router-link to="/admin/restaurants">Restaurants</router-link> |
-    <router-link to="/admin/categories">Categories</router-link> |
-    <router-link to="/admin/users">Users</router-link>
-  </div>
-</template>
